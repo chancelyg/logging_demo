9f8850a092a2b3488ae8356f584a34614e824eb0
chancel <ycs1026@vip.qq.com>
2020-07-19 22:58:11 +0800

    [IMP] 日志写入到Logs文件夹中

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import logging
 import logging.config
 import yaml
+import os
 from business.business import ShopingCart
+
+if not os.path.exists('logs'):
+    os.mkdir('logs')
 
 with open('logging.yaml','r',encoding='utf-8') as f:
     config = yaml.load(f)
